0de61f0605601821093e7e1253c4d072d34b20d7
sz48726 <sz48726@ly.com>
2019-12-24 23:16:20 +0800

    add 注释

--- spring-study/src/main/java/com/sun/zq/MyApp.java
+++ spring-study/src/main/java/com/sun/zq/MyApp.java
@@ -2,9 +2,11 @@ package com.sun.zq;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @ComponentScan("com.sun.zq")
 @Configuration
+@EnableAspectJAutoProxy(proxyTargetClass = true)
 public class MyApp {
 
 }
