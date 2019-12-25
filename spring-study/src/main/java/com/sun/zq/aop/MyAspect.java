d236162a471144f6777a84f91d07415eacf165dc
sz48726 <sz48726@ly.com>
2019-12-25 21:25:16 +0800

    aop提交

--- spring-study/src/main/java/com/sun/zq/aop/MyAspect.java
+++ spring-study/src/main/java/com/sun/zq/aop/MyAspect.java
@@ -1,5 +1,6 @@
 package com.sun.zq.aop;
 
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -9,14 +10,25 @@ import org.springframework.stereotype.Component;
 @Aspect
 public class MyAspect {
 
-	@Pointcut("execution(* com.sun.zq..*.*(..))")
+	@Pointcut("execution(* com.sun.zq.aop..*.*(..))")
 	public void pointCut(){
+		System.out.println("point cut execute");
+	}
 
+	@Pointcut("execution(* com.sun.zq.aop..*.*(..))")
+	public void pointCutAfter(){
+		System.out.println("point cut execute");
 	}
 
 	// advice在pointCut之前执行
 	@Before("com.sun.zq.aop.MyAspect.pointCut()")
 	public void advice() {
 		System.out.println("before......");
+	}
+
+	// advice在pointCut之前执行
+	@After("pointCutAfter()")
+	public void adviceAfter() {
+		System.out.println("after......");
 	}
 }
